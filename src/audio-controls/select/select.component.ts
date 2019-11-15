5cd2591864687cd11ee9104f69798f8ca3e24337
Greg Friedlander <C-Greg.Friedlander@charter.com>
2019-11-15 09:38:16 -0700

    cleaning up osc comp, adding nexus select

--- src/audio-controls/select/select.component.ts
+++ src/audio-controls/select/select.component.ts
@@ -1,16 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, EventEmitter, Input, OnInit, Output, Renderer2, TemplateRef, ViewChild } from '@angular/core';
+import { debug } from 'util';
+import { Theme } from '../../shared/theme';
 
+declare var Nexus: any;
+declare var Tone: any;
 @Component({
   selector: 'app-select',
   templateUrl: './select.component.html',
   styleUrls: ['./select.component.scss']
 })
-export class SelectComponent implements OnInit {
-
-  constructor() {
+export class SelectComponent implements OnInit, AfterViewInit {
+  @ViewChild('select', {static: false}) select: TemplateRef<any>;
+  @Input() id: string;
+  @Input() size: [number, number];
+  @Input() options: any[];
+  @Output() change = new EventEmitter();
+  theme = new Theme();
+  constructor(private renderer: Renderer2) {
   }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit(): void {
+    const comp = this;
+    Nexus.colors.dark = comp.theme.strokes.blue;
+    // debugger;
+
+    const newSelect = new Nexus.Select(this.id, {
+      'size': this.size || [40, 20],
+      'options': this.options
+    });
+
+    this.select = newSelect;
+    // this.select['colors']['fill'] = 'white';
+    // this.renderer.setStyle(this.select.elementRef.nativeElement, 'color', 'white');
+
+    newSelect.on('change', (value?) => {
+      comp.change.emit(value.value);
+    });
   }
 
 }
--- src/module/oscillator/oscillator.component.ts
+++ src/module/oscillator/oscillator.component.ts
@@ -11,9 +11,26 @@ export class OscillatorComponent implements OnInit {
   osc;
   cvIn;
   currentType = 0;
-  types = ["sine4", "sawtooth4", "square4", "triangle4"];
   currentClass = 0;
-  classes = ['simple', 'fat', 'pwm', 'fm', 'am', 'pulse'];
+  // types = ['sine4', 'sawtooth4', 'square4', 'triangle4'];
+  // classes = ['simple', 'fat', 'pwm', 'fm', 'am', 'pulse'];
+  public oscType = [
+    '',
+    'am',
+    'fm',
+    'fat',
+    'pulse',
+    'pwm'
+  ];
+  public waveTypes = [
+    'sine',
+    'square',
+    'sawtooth',
+    'triangle',
+  ];
+  private oscTypeString = '';
+  private waveTypeString = 'sine';
+  private count = 0;
 
   constructor() {
     this.osc = new Tone.OmniOscillator().start();
@@ -24,40 +41,76 @@ export class OscillatorComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.constructOscType(this.oscTypeString, this.waveTypeString);
   }
 
-  updateType() {
-    switch (this.currentClass) {
-      case 1:
-        this.osc.type = 'fat'+this.types[this.currentType];
-        break;
-      case 2:
-        this.osc.type = 'pwm';
-        break;
-      case 3:
-        this.osc.type = 'fm'+this.types[this.currentType];
-        break;
-      case 4:
-        this.osc.type = 'am'+this.types[this.currentType];
-        break;
-      case 5:
-        this.osc.type = 'pulse';
-        break;
-      default:
-      case 0:
-        this.osc.type = this.types[this.currentType];
-        break;
+  // updateType() {
+  //   switch (this.currentClass) {
+  //     case 1:
+  //       this.osc.type = 'fat' + this.types[this.currentType];
+  //       break;
+  //     case 2:
+  //       this.osc.type = 'pwm';
+  //       break;
+  //     case 3:
+  //       this.osc.type = 'fm' + this.types[this.currentType];
+  //       break;
+  //     case 4:
+  //       this.osc.type = 'am' + this.types[this.currentType];
+  //       break;
+  //     case 5:
+  //       this.osc.type = 'pulse';
+  //       break;
+  //     default:
+  //     case 0:
+  //       this.osc.type = this.types[this.currentType];
+  //       break;
+  //   }
+  // }
+
+  private constructOscType(oscType: string, waveType: string) {
+    // if oscType is not selected, it creates a basic synth
+    if (!oscType) {
+      this.osc.type = waveType;
+      console.log('osc.type: ', this.osc.type);
+    } else if (oscType === 'pwd') {
+      this.osc.type = oscType;
+      console.log('osc.type: ', this.osc.type);
+    } else {
+      this.osc.type = oscType + waveType;
+      console.log('osc.type: ', this.osc.type);
     }
+
   }
 
-  setType(i) {
-    this.currentType = i;
-    this.updateType();
+  setOscType(i) {
+    // if (this.count === 0) {
+    //   this.oscTypeString = i;
+    //   this.constructOscType(this.oscTypeString, this.waveTypeString);
+    // }
+    // this.count++;
+    this.checkValidOscType(i);
+    console.log('oscType: ', i);
+    // this.currentType = i;
+    // this.osc.type = i;
+    // this.updateType();
   }
 
-  setClass(i) {
-    this.currentClass = i;
-    this.updateType();
+  setWaveType(i) {
+    this.waveTypeString = this.waveTypes[i];
+    this.constructOscType(this.oscTypeString, this.waveTypeString);
+    // this.currentClass = i;
+    // this.updateType();
+  }
+
+  private checkValidOscType(value) {
+    for (var i = 0; i < this.oscType.length; i++) {
+      if (value.indexOf(this.oscType[i]) > -1) {
+        console.log('hit TRUE')
+        return true;
+      }
+    }
+    return false;
   }
 
 }
